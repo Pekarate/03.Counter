e5dd334b501d68ea2fdf457e7564080010f35312
Pekarate <48339214+Pekarate@users.noreply.github.com>
2023-08-20 13:55:22 +0700

    modify sensor value

--- src/vcnl36821.c
+++ src/vcnl36821.c
@@ -337,12 +337,14 @@ UINT8 set_PS_I_VCSEL(UINT8 i_vcsel)
 #define PS_AC_PERIOD (0)
 #define PS_AC_NUM (3 << 4)
 
+#define LEDI_144mA 0x0E
+#define LEDI_156mA 0x0F
 void VCNL_initialize(void)
 {
   // clean config bytes
   VCNL36821_Write_register(VCNL_PS_CONF1,0x01,0x00);
-  VCNL36821_Write_register(VCNL_PS_CONF2,0xC0,0xD0);
-  VCNL36821_Write_register(VCNL_PS_CONF3,0x00,0x0E);//config 3,4
+  VCNL36821_Write_register(VCNL_PS_CONF2,0xF0,0xE0);
+  VCNL36821_Write_register(VCNL_PS_CONF3,0x00,LEDI_156mA);//config 3,4
   VCNL36821_Write_register(VCNL_PS_THDL,0x00,0x00);//
   VCNL36821_Write_register(VCNL_PS_THDH,0xFF,0x0F);//
   VCNL36821_Write_register(VCNL_PS_CANC,0x00,0x00);//
